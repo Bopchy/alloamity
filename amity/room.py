cf81be63d01c0d08c6622b6d98a0ca24d1886f99
Bopchy <ruth.ogendi@andela.com>
2016-10-09 22:16:15 +0300

    #131959101 Adds database. Adds database with required columns.

--- amity/room.py
+++ amity/room.py
@@ -1,14 +1,23 @@
+from ..models.amity_database import Room, sess
+
 class Room(object):
-    """
+    """ Class that can create rooms, randomly add people to rooms,
+        check rooms for available space 
     """
     def __init__(self):
-    	pass
+        self.session = sess
+    	
 
-    def create_room(self, room_name, room_type):
-    	pass
+    def create_room(self, room_name, room_type, room_capacity):
+    	new_room = Room()
+        new_room.room_name = room_name
+        new_room.room_type = room_type
+        new_room.room_capacity = room_capacity
+        sess.add(new_room)
+        sess.commit(new_room)
 
     def space_available():
-    	pass
+    	
 
     def add_person(self, person_id, room_name):
     	pass
--- models/amity_database.py
+++ models/amity_database.py
@@ -1,31 +1,35 @@
 import os
 import sys
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, String 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine 
+from sqlalchemy.orm import sessionmaker
 
-Base = declarative_base() 
+Base = declarative_base()
+db = create_engine('sqlite:////opt/projects/checkpoint_1/cp1.db') 
+DBSession = sessionmaker() 
+sess = DBSession() # Creates session object
+DBSession.configure(bind=db) # Binds the db session to the engine
 
-class Amity(Base):
-	"""General database for Amity Office Allocation system"""
-	__tablename__= "amity"
-		id = Column(Integer, primary_key=True)
-		
-		def print_allocations():
-			pass
 
 class Person(Base):
 	"""Database containing Fellows and Staff"""
 	__tablename__= "person"
-	id = Column(Integer, primary_key=True)
+	person_id = Column(Integer, primary_key=True)
+	first_name = Column(String(15), index=True)
+	last_name = Column(String(15))
+	gender = Column(String(1)) # Restrict to either female or male
+	job_group = Column(String(7), index=True)
+	assigned_office = Column(String(15), index=True)
+	assigned_living_space = Column(String(15), nullable=True, index=True)
 
 class Room(Base):
 	"""Database containing information on Living spaces and Offices"""
 	__tablename__= "room"
 	room_name = Column(String, primary_key=True)
+	room_type = Column(String, index=True, )
+	room_capacity = Column(Integer)
+	room_occupants = Column(Integer)		
 
-
-		
-		
-		
-
+# Creates all the tables defined by models
+Base.metadata.create_all(db)
