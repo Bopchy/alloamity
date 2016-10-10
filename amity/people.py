c0974ad37345b463786d422e50be1fd5b70844f1
Bopchy <ruth.ogendi@andela.com>
2016-10-10 23:13:29 +0300

    #131966501 Fixes add_person(). Refactors add_person() and fixes errors that were present.

--- amity/people.py
+++ amity/people.py
@@ -1,4 +1,5 @@
 from models.amity_database import sess, Person as PersonClass
+from room import Room
 
 class Person(object):
 	"""docstring for People"""
@@ -9,9 +10,17 @@ class Person(object):
 		pass
 
 	def new_employee(self, first_name, last_name, job_group, want_accomodation, gender):
-		pass
+		new_person = PersonClass()
+		new_person.first_name = first_name
+		new_person.last_name = last_name
+		new_person.job_group = job_group
+		new_person.want_accomodation = want_accomodation
+		new_person.gender = gender
 
+		Room().add_person(new_person)
 
-
+		sess.add(new_person)
+		sess.commit()
 	
-		
+p1 = Person()
+p1.new_employee('Ruth', 'Bochere', 'Fellow', 'Y', 'F')		
--- amity/room.py
+++ amity/room.py
@@ -1,5 +1,5 @@
-from models.amity_database import Room as RoomClass, sess
-from people import Person 
+from models.amity_database import Room as RoomClass, Person as PersonClass, sess 
+# from people import Person
 import random  
 
 class Room(object):
@@ -7,12 +7,9 @@ class Room(object):
         check rooms for available space and reallocate people to different
         rooms 
     """
+    # self.session = sess
     list_of_offices = []
-    list_of_living_spaces = [] 
-
-    def __init__(self):
-        # self.session = sess
-        pass
+    list_of_living_spaces = []
 
     def create_room(self, room_name, room_type, room_capacity):
         new_room = RoomClass()
@@ -23,25 +20,40 @@ class Room(object):
         sess.add(new_room)
         sess.commit()
 
-    def space_available(self):
-        sa = sess.query(RoomClass).filter(RoomClass.room_name == room_name).one()
+    def space_available(self, room_name):
+        sa = sess.query(RoomClass).filter_by(room_name=room_name).one()
         if sa.room_occupants < sa.room_capacity:
             return True # There is space 
         return False # No space available 
 
-    def add_person(self):
-        room = Room() 
-        offices = sess.query(RoomClass).filter(RoomClass.room_type == 'Office').all()
-        list_of_offices.append(offices)
-        for office in list_of_offices:
-            is_there_space = room.space_available()
-            if is_there_space:
-                new_person.assigned_office = random.choice(list_of_offices) 
-        sess.add(new_person.assigned_office)
-        sess.commit()
+    def add_person(self, new_person):         
+        self.list_of_offices = sess.query(RoomClass).filter_by(room_type='Office').all()
+                        
+        for office in self.list_of_offices:
+            is_there_office_space = self.space_available(office.room_name)
+
+            if is_there_office_space:
+                new_person.assigned_office = random.choice(self.list_of_offices).room_name # Is assigning an object from list of room objects queries 
+                sess.query(RoomClass)\
+                    .filter_by(room_name=new_person.assigned_office)\
+                    .update({'room_occupants': RoomClass.room_occupants+1})
+
+        if new_person.job_group == 'Fellow' and new_person.want_accomodation == 'Y':
+            self.list_of_living_spaces = sess.query(RoomClass).filter_by(room_type='Living Space').all()
+            for living_space in self.list_of_living_spaces:
+                is_there_living_space = self.space_available(living_space.room_name)
+                if is_there_living_space:
+                    new_person.assigned_living_space = random.choice(self.list_of_living_spaces).room_name
+                    sess.query(RoomClass).filter_by(room_name=new_person.assigned_living_space)\
+                        .update({'room_occupants': RoomClass.room_occupants+1})
 
     def reallocate_person():
-    	pass
+    	
 
-r1 = Room()
-r1.create_room('Narnia', 'Office', 6)
+
+# r1 = Room()
+# r1.create_room('Narnia', 'Office', 6)
+# r2 = Room()
+# r2.create_room('Krypton', 'Office', 6)
+# r3 = Room()
+# r3.create_room('Shire', 'Office', 6)
