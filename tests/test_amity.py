8eb28ac0e57712b3672ba22749db72f396f3d824
Ruth Ogendi <ruth.ogendi@andela.com>
2016-11-01 18:34:25 +0300

    Refactor code git stash commit.

--- tests/test_amity.py
+++ tests/test_amity.py
@@ -1,27 +1,23 @@
-from app.amity import Amity  
 
-import unittest
-from unittest.mock import MagicMock, patch
 
 class TestAmity(unittest.TestCase):
-	"""Tests class Amity
-	"""
-	def setUp(self):
-		pass
 
-	def test_Amity_is_a_class(self):
-		a = Amity()
-		self.assertIsInstance(a, Amity)
-	
-	def test_amity_prints_allocations(self):
-		pass
+    """Tests class Amity
+    """
 
-	def tests_amity_prints_unallocated(self):
-		pass
+    def setUp(self):
+        pass
 
-	@patch('test_amity.load_state')
-	def tests_amity_loads_state(self, mock_load_state):
-		pass	
+    def tests_amity_creates_room(self):
 
-	def tests_amity_saves_state(self):
-		pass
+    def test_amity_prints_allocations(self):
+        pass
+
+    def tests_amity_prints_unallocated(self):
+        pass
+
+    def tests_amity_loads_state(self, mock_load_state):
+        pass
+
+    def tests_amity_saves_state(self):
+        pass
