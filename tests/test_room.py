129d02e5e86ed491aa1a51f384bfdaa4d5755d71
Ruth Ogendi <ruth.ogendi@andela.com>
2016-11-01 18:34:25 +0300

    Refactor code git stash commit.

--- tests/test_room.py
+++ tests/test_room.py
@@ -2,32 +2,29 @@ import unittest
 import mock
 from app.room import Room
 from app.people import Person
-from tests.test_database import CreateTestDatabase  
+from tests.test_database import CreateTestDatabase
+
 
 class TestRoom(unittest.TestCase):
-	"""docstring for TestRoom
-	"""
-	def setUp(self):
-		""" Sets up test data
-		"""
-		self.room = Room()
-		# self.office_max = 6
-		# self.livingspace_max = 4
-		self.new_person = Person() 
 
-	def tests_room_creates_new_rooms(self, room_name, room_type):
-		room_name = 'War Room'
-		room_type = 'Office'
+    """docstring for TestRoom
+    """
 
-		
+    def setUp(self):
+        """ Sets up test data
+        """
+        self.room = Room()
+        # self.office_max = 6
+        # self.livingspace_max = 4
+        self.new_person = Person()
 
-	def tests_rooms_check_for_available_space(self):
-		pass
+    def tests_rooms_check_for_available_space(self):
+        pass
 
-	def tests_room_adds_person(self):
+    def tests_room_adds_person(self):
 
-		self.room.add_person(self.new_person)
-		self.assertIn(self.new_person, new_office)
+        self.room.add_person(self.new_person)
+        self.assertIn(self.new_person, new_office)
 
-	def tests_room_reallocates_person(self):
-		pass
+    def tests_room_reallocates_person(self):
+        pass
