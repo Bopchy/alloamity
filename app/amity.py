5405be3b1c364489c0316a3306922fd4b36ca205
Ruth Ogendi <ruth.ogendi@andela.com>
2016-11-02 19:11:06 +0300

    Formats codebase through pep8.

--- app/amity.py
+++ app/amity.py
@@ -49,7 +49,13 @@ class Amity(object):
         new_room.room_occupants = 0
         self.session.add(new_room)
 
-    def add_person(self, first_name, last_name, job_group, want_accomodation='N', gender='N/A'):
+    def add_person(
+            self,
+            first_name,
+            last_name,
+            job_group,
+            want_accomodation='N',
+            gender='N/A'):
         new_person = Person()
         new_person.first_name = first_name
         new_person.last_name = last_name
@@ -69,7 +75,9 @@ class Amity(object):
         self.list_of_offices = self.rooms.filter_by(room_type='O').all()
 
         for office in self.list_of_offices:
-            is_there_office_space = super(Office, self).space_available(office.room_name)
+            is_there_office_space = super(
+                Office, self).space_available(
+                office.room_name)
             if is_there_office_space:
                 the_offices.append(office)
 
@@ -78,7 +86,7 @@ class Amity(object):
         # new_person.assigned_office
 
         rooms.filter_by(room_name=new_person.assigned_office)\
-            .update({'room_occupants': Room.room_occupants+1})
+            .update({'room_occupants': Room.room_occupants + 1})
 
     def add_person_to_living_space(self, new_person):
         the_living = []
@@ -93,7 +101,7 @@ class Amity(object):
         new_person.assigned_living_space = random.choice(
             self.list_of_living_spaces).room_name
         rooms.filter_by(room_name=new_person.assigned_living_space)\
-            .update({'room_occupants': RoomModel.room_occupants+1})
+            .update({'room_occupants': RoomModel.room_occupants + 1})
         pass
 
     @staticmethod
@@ -104,7 +112,8 @@ class Amity(object):
                 person = line.replace('\n', '').split()
                 print(person)
                 Room().add_person(*person)
-                # Using python splat to add members of list as arguments to add_person()
+                # Using python splat to add members of list as arguments to
+                # add_person()
 
     def print_room(self, room_name):
         members_in_office = self.persons.filter_by(
@@ -113,7 +122,8 @@ class Amity(object):
             assigned_living_space=room_name).all()
         members_in_room = members_in_office + members_in_living_space
 
-        [print(member.first_name + ' ' + member.last_name) for member in members_in_room]
+        [print(member.first_name + ' ' + member.last_name)
+         for member in members_in_room]
 
     def print_allocations(self):
         existing_rooms = self.rooms.all()
@@ -121,26 +131,33 @@ class Amity(object):
             print('There are currently no existing rooms.')
         for room in existing_rooms:
             if room.room_type == 'O':
-                members_of_the_room = self.persons.filter_by(assigned_office=room.room_name).all()
+                members_of_the_room = self.persons.filter_by(
+                    assigned_office=room.room_name).all()
                 print(room.room_name)
                 print('-' * 30)
                 if len(members_of_the_room) == 0:
                     print('This room is empty')
-                member_names = [member.first_name + ' ' + member.last_name for member in members_of_the_room]
+                member_names = [
+                    member.first_name +
+                    ' ' +
+                    member.last_name for member in members_of_the_room]
                 print(', '.join(member_names) + '\n')
 
             else:
-                members_of_the_room = self.persons.filter_by(assigned_living_space=room.room_name).all()
+                members_of_the_room = self.persons.filter_by(
+                    assigned_living_space=room.room_name).all()
                 print(room.room_name)
                 print('-' * 30)
                 if len(members_of_the_room) == 0:
                     print('This room is empty')
-                member_names = [member.first_name + ' ' + member.last_name for member in members_of_the_room]
+                member_names = [
+                    member.first_name +
+                    ' ' +
+                    member.last_name for member in members_of_the_room]
                 print(', '.join(member_names) + '\n')
 
     def reallocate_person(self, person_id, room_name):
         person_details = persons.filter_by(person_id=person_id).one()
-        # person_job_group =
         room_details = rooms.filter_by(room_name=room_name).one()
         room_type = room_details.room_type
         space_status = Room().space_available(room_name)
@@ -150,26 +167,29 @@ class Amity(object):
                 persons.filter_by(person_id=person_id).update(
                     {'assigned_office': room_name})
                 rooms.filter_by(room_name=room_name).update(
-                    {'room_occupants': room_details.room_occupants+1})
+                    {'room_occupants': room_details.room_occupants + 1})
                 rooms.filter_by(room_name=current_room).update(
-                    {'room_occupants': RoomModel.room_occupants-1})
+                    {'room_occupants': RoomModel.room_occupants - 1})
 
             elif room_type == 'L':
                 current_room = person_details.assigned_living_space
                 persons.filter_by(person_id=person_id).update(
                     {'assigned_living_space': room_name})
                 rooms.filter_by(room_name=room_name).update(
-                    {'room_occupants': room_details.room_occupants+1})
+                    {'room_occupants': room_details.room_occupants + 1})
                 rooms.filter_by(room_name=current_room).update(
-                    {'room_occupants': RoomModel.room_occupants-1})
+                    {'room_occupants': RoomModel.room_occupants - 1})
 
-    def print_unallocated(self):  # Prints fellows that said N to want_accomodation
+    def print_unallocated(self):
         unallocated = self.persons.filter_by(want_accomodation='N').all()
         if len(unallocated) == 0:
             print('There are currently no unallocated fellows.')
             return
 
-        unallocated_fellows = [fellow.first_name + ' ' + fellow.last_name for fellow in unallocated]
+        unallocated_fellows = [
+            fellow.first_name +
+            ' ' +
+            fellow.last_name for fellow in unallocated]
 
         for fellow in unallocated_fellows:
             print(fellow)
@@ -177,12 +197,15 @@ class Amity(object):
     def exit_room(self, person_id, room_name):
         person_details = persons.filter_by(person_id=person_id).one()
         person_office_details = persons.filter(assigned_office)
-        person_living_space_details = person_details.filter(assigned_living_space)
+        person_living_space_details = person_details.filter(
+            assigned_living_space)
         try:
             if person_office_details is room_name:
-                persons.filter_by(assigned_office=room_name).update({'assigned_office': 'NULL'})
+                persons.filter_by(assigned_office=room_name).update(
+                    {'assigned_office': 'NULL'})
             if person_living_space_details is room_name:
-                persons.filter_by(assigned_living_space=room_name).update({'assigned_living_space': 'NULL'})
+                persons.filter_by(assigned_living_space=room_name).update(
+                    {'assigned_living_space': 'NULL'})
         except Exception:
             return 'Person' + person_id + 'is not in that room.'
 
